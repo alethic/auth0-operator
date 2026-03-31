f8fa819d98f222d6b33800f27564eb61c166c4f5
Jerome Haltom <jhaltom@alethic.solutions>
2026-03-30 20:22:41 -0500

    rename dir?

--- src/Alethic.Auth0.Operator/Converters/TenantConverter.cs
+++ src/Alethic.Auth0.Operator/Converters/TenantConverter.cs
@@ -20,7 +20,6 @@ namespace Alethic.Auth0.Operator.Converters
     {
 
         protected override IEnumerable<IEntityConverter<V2alpha1Tenant>> Converters => [
-            new V1alpha1ToV2alpha1(),
             new V1ToV2alpha1()
         ];
 
@@ -48,38 +47,6 @@ namespace Alethic.Auth0.Operator.Converters
                 result.Spec.Policy = from.Spec.Policy;
                 result.Spec.Name = from.Spec.Name;
                 result.Spec.Auth = from.Spec.Auth is { } auth ? new V1Tenant.SpecDef.AuthDef() { SecretRef = auth.SecretRef, Domain = auth.Domain } : null;
-                result.Spec.Init = JsonSerializer.Deserialize<V1TenantConf?>(JsonSerializer.Serialize(from.Spec.Init?.Settings));
-                result.Spec.Conf = JsonSerializer.Deserialize<V1TenantConf?>(JsonSerializer.Serialize(from.Spec.Conf?.Settings));
-                result.Status.LastConf = JsonSerializer.Deserialize<Hashtable?>(JsonSerializer.Serialize(from.Status.LastConf?.Settings));
-                return result;
-            }
-
-        }
-
-        /// <summary>
-        /// Converts from <see cref="V1alpha1Tenant"/> to <see cref="V2alpha1Tenant"/>.
-        /// </summary>
-        class V1alpha1ToV2alpha1 : IEntityConverter<V1alpha1Tenant, V2alpha1Tenant>
-        {
-
-            public V2alpha1Tenant Convert(V1alpha1Tenant from)
-            {
-                var result = new V2alpha1Tenant { Metadata = from.Metadata };
-                result.Spec.Policy = from.Spec.Policy;
-                result.Spec.Name = from.Spec.Name;
-                result.Spec.Auth = from.Spec.Auth is { } auth ? new V2alpha1Tenant.SpecDef.AuthDef() { SecretRef = auth.SecretRef, Domain = auth.Domain } : null;
-                result.Spec.Init = new V2alpha1TenantConf() { Settings = JsonSerializer.Deserialize<TenantSettings?>(JsonSerializer.Serialize(from.Spec.Init)) };
-                result.Spec.Conf = new V2alpha1TenantConf() { Settings = JsonSerializer.Deserialize<TenantSettings?>(JsonSerializer.Serialize(from.Spec.Conf)) };
-                result.Status.LastConf = new V2alpha1TenantConf() { Settings = JsonSerializer.Deserialize<TenantSettings?>(JsonSerializer.Serialize(from.Status.LastConf)) };
-                return result;
-            }
-
-            public V1alpha1Tenant Revert(V2alpha1Tenant from)
-            {
-                var result = new V1alpha1Tenant { Metadata = from.Metadata };
-                result.Spec.Policy = from.Spec.Policy;
-                result.Spec.Name = from.Spec.Name;
-                result.Spec.Auth = from.Spec.Auth is { } auth ? new V1alpha1Tenant.SpecDef.AuthDef() { SecretRef = auth.SecretRef, Domain = auth.Domain } : null;
                 result.Spec.Init = JsonSerializer.Deserialize<V1TenantConf?>(JsonSerializer.Serialize(from.Spec.Init?.Settings));
                 result.Spec.Conf = JsonSerializer.Deserialize<V1TenantConf?>(JsonSerializer.Serialize(from.Spec.Conf?.Settings));
                 result.Status.LastConf = JsonSerializer.Deserialize<Hashtable?>(JsonSerializer.Serialize(from.Status.LastConf?.Settings));
