d2e6ece14bbaf95d74e7467beac624ddb9fc54ee
Jerome Haltom <jhaltom@alethic.solutions>
2026-05-18 00:18:09 -0500

    Added current V1 version back.

---
+++ src/Alethic.Auth0.Operator.Core/Models/Connection/V1/V1ConnectionChallengeUi.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Alethic.Auth0.Operator.Core.Models.Connection.V1
+{
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum V1ConnectionChallengeUi
+    {
+
+        [JsonStringEnumMemberName("both")]
+        Both,
+
+        [JsonStringEnumMemberName("autofill")]
+        AutoFill,
+
+        [JsonStringEnumMemberName("button")]
+        Button
+
+    }
+
+}
---
+++ src/Alethic.Auth0.Operator.Core/Models/Connection/V1/V1ConnectionSetUserRootAttributes.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Alethic.Auth0.Operator.Core.Models.Connection.V1
+{
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum V1ConnectionSetUserRootAttributes
+    {
+
+        [JsonStringEnumMemberName("on_each_login")]
+        OnEachLogin,
+
+        [JsonStringEnumMemberName("on_first_login")]
+        OnFirstLogin,
+
+        [JsonStringEnumMemberName("never_on_login")]
+        NeverOnLogin
+
+    }
+
+}
